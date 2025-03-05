28bdeaf6ffd757438d0a539838e6ead04c0add3d
Alan Gallauresi <alan.gallauresi@gmail.com>
2025-03-05 10:59:21 -0500

    4

--- prov.sh
+++ prov.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Ensure system updates & required packages
-apt-get update && apt-get install -y curl
-
-# Set working directory
-cd /workspace/webui || exit 1
-
-# Ensure necessary directories exist
-mkdir -p /workspace/webui/pipelines
-
-# Start Pipelines in the same process (no background mode)
-( /app/pipelines & sleep 5; exec /app/open-webui )
+touch /var/tmp/vast-test-success
